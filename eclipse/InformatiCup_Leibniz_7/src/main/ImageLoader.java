70ae7a037452c1d0e6f03dd1f4302409a627b9e4
JannikG <jannikgroeger@gmx.net>
2018-10-11 11:41:58 +0200

    Implemented ImageLoader and ImageSaver and added Tests

--- eclipse/InformatiCup_Leibniz_7/src/main/ImageLoader.java
+++ eclipse/InformatiCup_Leibniz_7/src/main/ImageLoader.java
@@ -1,5 +1,90 @@
 package main;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+
+/**
+ * This class provides functionality to load Images from the file system
+ * Compatible image formats are: .bmp .jpeg .wbmp .gif .png .jpg
+ * 
+ * @author Jannik
+ *
+ */
 public class ImageLoader {
+
+	/**
+	 * This methods loads an image from the file system and returns a buffered Image
+	 * object
+	 * 
+	 * @param path
+	 *            The path to the file(including file name and extension)
+	 * @return An BufferedImage Object containing the image data
+	 * @throws IOException
+	 *             If Image could not be loaded(wrong path, no read permission)
+	 */
+	public static BufferedImage loadImage(String path) throws IOException {
+
+		// Check for null parameters
+		if (path == null) {
+			throw new NullPointerException("Given path string is null!");
+		}
+
+		BufferedImage img;
+		img = ImageIO.read(new File(path));
+
+		//ImageIO Returns null for unsupported Formats
+		if(img == null) {
+			throw new IOException("Unsupported Image Format!");
+		}
+		
+		return img;
+	}
+
+	/**
+	 * This method loads all images in a folder. It will ignore directories in the
+	 * folder, but will attempt to load every single file.
+	 * 
+	 * @param folderPath
+	 *            The folder with the image files
+	 * @return an ArrayList filled with BufferedImages from the folder(no guaranteed
+	 *         order, but likely alphabetical)
+	 * @throws IOException
+	 *             When the folderpath is not valid or folder contains an not as
+	 *             image loadable file
+	 */
+	public static List<BufferedImage> loadImagesFromFolder(String folderPath) throws IOException {
+
+		List<BufferedImage> list = new ArrayList<BufferedImage>();
+
+		// Add handle for the folder and read out filelist
+		File folder = new File(folderPath);
+		File[] listOfFiles = folder.listFiles();
+
+		// Check for invalid folderpath
+		if (listOfFiles == null) {
+			throw new IOException("Path does not lead to a valid folder");
+		}
+
+		// Attempt to load every file in folder
+		for (int i = 0; i < listOfFiles.length; i++) {
+			if (listOfFiles[i].isFile()) {
+				BufferedImage img = ImageIO.read(listOfFiles[i]);
+				
+				//ImageIO Returns null for unsupported Formats
+				if(img == null) {
+					throw new IOException("Unsupported Image Format!");
+				}
+				
+				list.add(img);
+			}
+		}
+
+		return list;
+	}
 
 }
--- eclipse/InformatiCup_Leibniz_7/src/main/ImageSaver.java
+++ eclipse/InformatiCup_Leibniz_7/src/main/ImageSaver.java
@@ -1,5 +1,103 @@
 package main;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+
+/**
+ * This class contains methods to save images to the file system
+ * 
+ * @author Jannik
+ *
+ */
 public class ImageSaver {
+
+	// Constants for the supported Image formats
+	public static final String PNG = "png";
+	public static final String GIF = "gif";
+	public static final String jpg = "jpg";
+
+	/**
+	 * This method saves an given BufferedImage as an image file in the filesystem
+	 * 
+	 * @param img
+	 *            The Image to be saved
+	 * @param savePath
+	 *            Path where it will be saved(including filename but no extension!)
+	 * @param format
+	 *            The format in which the file will be saved(use the constants of
+	 *            this class)
+	 * @throws IOException
+	 *             When image could not be saved(path does not exist, no write
+	 *             permissions)
+	 */
+	public static void saveImage(BufferedImage img, String savePath, String format) throws IOException {
+
+		File outputfile = new File(savePath + "." + format);
+
+		// ImageIO write returns false when image could not be saved
+		boolean success = ImageIO.write(img, format, outputfile);
+
+		if (!success) {
+			throw new IOException("Could not save Image!");
+		}
+	}
+
+	/**
+	 * This method saves an given BufferedImage as an image file in the filesystem,
+	 * default type is png
+	 * 
+	 * @param img
+	 *            The Image to be saved
+	 * @param savePath
+	 *            Path where it will be saved(including filename but no extension!)
+	 * @throws IOException
+	 *             When image could not be saved(path does not exist, no write
+	 *             permissions)
+	 */
+	public static void saveImage(BufferedImage img, String savePath) throws IOException {
+
+		File outputFile = new File(savePath + "." + PNG);
+
+		// ImageIO write returns false when image could not be saved
+		boolean success = ImageIO.write(img, PNG, outputFile);
+
+		if (!success) {
+			throw new IOException("Could not save Image!");
+		}
+	}
+
+	/**
+	 * This method saves all Images from a List into a folder. The images will be
+	 * numbered after their order in the list
+	 * 
+	 * @param list
+	 *            The list of bufferedImages to save
+	 * @param savePath
+	 *            The path and filename for the files, using no extension!
+	 * @param format
+	 *            Format in which the images will be saved, use the constants of
+	 *            this class.
+	 * @throws IOException
+	 *             When images could not be saved(path does not exist, no write
+	 *             permissions)
+	 */
+	public static void saveImagesFromList(List<BufferedImage> list, String savePath, String format) throws IOException {
+
+		for (int i = 0; i < list.size(); i++) {
+			BufferedImage img = list.get(i);
+			File outputFile = new File(savePath + i + "." + format);
+
+			// ImageIO write returns false when image could not be saved
+			boolean success = ImageIO.write(img, format, outputFile);
+
+			if (!success) {
+				throw new IOException("Could not save Image(" + i + ")!");
+			}
+		}
+	}
 
 }
