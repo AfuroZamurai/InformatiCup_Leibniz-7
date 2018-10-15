762a76784affcca21741e9cf9b07f9c54717fae6
bruder <bruder@bruder-Aspire-ES1-311>
2018-10-15 11:06:23 +0200

    started drawing basic frame of signs using java graphics

--- eclipse/InformatiCup_Leibniz_7/src/picture/Border.java
+++ eclipse/InformatiCup_Leibniz_7/src/picture/Border.java
@@ -7,4 +7,17 @@ public class Border {
 	private Color color; //Color of the border
 	private int width; //border width
 	
+	public Border(Color color, int width) {
+		this.color = color;
+		this.width = width;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+	
 }
