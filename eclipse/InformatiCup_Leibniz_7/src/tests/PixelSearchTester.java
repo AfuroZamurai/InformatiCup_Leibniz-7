c5cbbbe13eeb95a92d95846c62c26fd34c3a336d
JannikG <jannikgroeger@gmx.net>
2018-10-25 13:15:04 +0200

    Added Tests for PixelSearchAlgorithm(WIP)

---
+++ eclipse/InformatiCup_Leibniz_7/src/tests/PixelSearchTester.java
@@ -0,0 +1,30 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+import main.evaluate.EvaluationResult;
+import main.evaluate.EvaluationResult.Sign;
+import main.gui.PixelSearchCancellationProcess;
+
+class PixelSearchTester {
+
+	@Test
+	void testPixelSearch() throws IOException {
+		
+		//Takes very long, adjust Filter size(when possible)
+		for(int i = 0; i < Sign.values().length; i++) {
+			
+			PixelSearchCancellationProcess pixelSearch = new PixelSearchCancellationProcess(Sign.values()[i]);
+
+			BufferedImage img = EvaluationResult.getExampleImage(Sign.values()[i]);
+			
+			BufferedImage result = pixelSearch.generateImage(img);
+			
+		}
+	}
+}
