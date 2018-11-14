0a8967ecc07532f7e80d91037f0e2ace8c849d24
Felix <felix.hampel@web.de>
2018-11-14 21:40:18 +0100

    test for cuckoo search

---
+++ eclipse/InformatiCup_Leibniz_7/src/tests/CuckooSearchTester.java
@@ -0,0 +1,25 @@
+package tests;
+
+import org.junit.Test;
+
+import main.CmdLineStart;
+import main.evaluate.Sign;
+import main.evolution.ga.CuckooSearch;
+import main.evolution.network.CPPN;
+import main.evolution.network.Config;
+
+public class CuckooSearchTester {
+	
+	//@Test
+	public void testCuckooSearch() {
+		CuckooSearch searcher = new CuckooSearch(new CPPN(0, 0, new Config(64, 64, 0, 50)), 30, 0.9f, 20, Sign.VORFAHRT);
+		searcher.searchForImage();
+	}
+	
+	@Test
+	public void testCmdLineCuckooSearch() throws Exception {
+		String[] args = new String[] {};
+		args = new String[] {"-a cuckoosearch"};
+		CmdLineStart.main(args);
+	}
+}
